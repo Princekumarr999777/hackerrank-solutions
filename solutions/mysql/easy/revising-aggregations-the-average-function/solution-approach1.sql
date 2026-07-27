7b37f348e2ac22bf6768468fcb8e12a262b5e062
Princekumarr999777 <110761058+Princekumarr999777@users.noreply.github.com>
2026-07-27 22:41:23 +0530

    ✅ Revising Aggregations - Averages [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-the-average-function/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+-- Problem     Revising Aggregations - Averages
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-27, 10:41 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT ='California';

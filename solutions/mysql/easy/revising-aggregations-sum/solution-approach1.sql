4a80feefb7a923dc693d4f2a689b4f5522bbf67c
Princekumarr999777 <110761058+Princekumarr999777@users.noreply.github.com>
2026-07-27 22:40:02 +0530

    ✅ Revising Aggregations - The Sum Function [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-sum/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+-- Problem     Revising Aggregations - The Sum Function
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-27, 10:39 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT ='California';
